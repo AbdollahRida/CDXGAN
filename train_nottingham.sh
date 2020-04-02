1ece692486a03ec65359b843d830cd7820043aee
Abdollah Rida <abdo@Abdollahs-MacBook-Pro.local>
2020-04-02 12:03:57 -0700

    Deleted old bash cmd

--- train_nottingham.sh
+++
@@ -1,8 +0,0 @@
-#mkdir data
-#cd data
-#wget http://www-labs.iro.umontreal.ca/~lisa/deep/data/Nottingham.zip
-#unzip -q Nottingham.zip
-#cd ..
-
-# CUDA_VISIBLE_DEVICES=2 (optinal)
-python3 main.py --pretrain_g_epochs 10 --total_epochs 5
